08eee964a2bed4a4d16c0cde4ec7f455603635f9
alexandrepalmag <alepalmag85ti@gmail.com>
2021-07-10 14:56:46 -0300

    Programming for launching the FrontController.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-echo 'hello';
+$url = substr($_SERVER['REQUEST_URI'], 1);
+
+$url = explode('/', $url);
+
+$controller = $url[0] && $url[0] ? $url[0] : 'page';
+
+var_dump($url);
